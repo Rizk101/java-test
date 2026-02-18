5469478e8121b8520791c2f9380b92459161ddc7
Rizk <krmrzk555@gmail.com>
2026-02-18 15:10:18 +0200

    Class

---
+++ 2.Data Base/task1/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+
+    private int id;
+    private String name;
+    private int age;
+
+    // Constructor
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method to print student data
+    public void printStudentInfo() {
+        System.out.println("Student ID: " + id);
+        System.out.println("Student Name: " + name);
+        System.out.println("Student Age: " + age);
+    }
+}
